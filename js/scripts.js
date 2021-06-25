b72b4512858abaaf579774d36175ca6e985c48bd
vitodigrigoli <domen-vit@hotmail.it>
2021-06-25 21:11:35 +0200

    add file

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+/* Dark Mode */
+const dark_icon = document.getElementById('dark-mode-icon')
+
+dark_icon.addEventListener('click', () =>{
+	console.log('click')
+	const section = document.getElementsByClassName('dark-mode--change')
+
+	for( let i=0; i < section.length; i++) {
+		section[i].classList.toggle('dark-mode')
+	}
+})
+
+const scroll = new LocomotiveScroll({
+	el: document.querySelector('[data-scroll-container]'),
+	smooth: true,
+	smartphone: {
+		smooth: true
+	},
+	tablet: {
+		smooth: true
+	},
+})
+
+scroll.on('call', func => {
+
+	const section = document.getElementsByClassName('panel')[0].classList.toggle(func)
+
+});
